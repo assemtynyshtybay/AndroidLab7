49db26ec61d57dbf0f0e01a73b13f03c76aa3137
assemtynyshtybay <asem201000@gmail.com>
2020-04-14 15:48:15 +0600

    lab7

--- JobDeveloper/app/src/main/java/com/example/jobdeveloper/MainActivity.java
+++ JobDeveloper/app/src/main/java/com/example/jobdeveloper/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabs = findViewById(R.id.tabs);
+        tabs.getTabAt(0).setIcon(R.drawable.ic_book);
+        tabs.getTabAt(1).setIcon(R.drawable.ic_fav);
+        tabs.getTabAt(2).setIcon(R.drawable.ic_more);
         //добавляем наш fragment jobs
         if (savedInstanceState == null) {
             FragmentManager fragmentManager = getFragmentManager();
